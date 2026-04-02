ed893aa4db0e49a08e942161890b010383bd4f09
globog3 <adipriyo45@gmail.com>
2026-04-02 16:30:26 +0700

    update

---
+++ latihan2/latihan.js
@@ -0,0 +1,11 @@
+const mahasiswa = {
+  nim: "22001",
+  nama: "Budi Santoso",
+  status: true,
+  matkul: [{ matkulid: "MK001", tugas: 85, UTS: 88, UAS: 90 }],
+};
+
+const mahasiswaList = [
+  { nim: "22001", nama: "Budi Santoso", status: true },
+  { nim: "22002", nama: "Siti Aminah", status: false },
+];
