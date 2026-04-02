03ce92430cbef534b6c2d397a284038940e4b636
globog3 <adipriyo45@gmail.com>
2026-04-02 17:15:40 +0700

    update

--- latihan2/latihan.js
+++ latihan2/latihan.js
@@ -1,6 +1,7 @@
 const mahasiswa = {
   nim: "22001",
   nama: "Budi Santoso",
+  umur: 21,
   status: true,
   matkul: [{ matkulid: "MK001", tugas: 85, UTS: 88, UAS: 90 }],
 };
@@ -9,3 +10,47 @@ const mahasiswaList = [
   { nim: "22001", nama: "Budi Santoso", status: true },
   { nim: "22002", nama: "Siti Aminah", status: false },
 ];
+
+console.log(mahasiswa);
+[{ nim: "22001", nama: "Budi Santoso", umur: 21, status: true }];
+
+const list_mahasiswa = [
+  {
+    nama: "mahasiswa 1",
+    nim: "A11.123123",
+    umur: 20,
+  },
+  {
+    nama: "mahasiswa 2",
+    nim: "A11.123124",
+    umur: 21,
+  },
+];
+
+console.log(list_mahasiswa);
+[
+  { nama: "mahasiswa 1", nim: "A11.123123", umur: 20 },
+  { nama: "mahasiswa 2", nim: "A11.123124", umur: 21 },
+];
+
+const mahasiswa1 = {
+  nim: "22001",
+  nama: "Budi Santoso",
+  umur: 21,
+  status: true,
+};
+
+const umur = mahasiswa1.umur;
+console.log(umur);
+21;
+
+if (umur > 21) {
+  console.log("yes tua");
+} else {
+  console.log("umur tidak pantasts");
+}
+
+const { nama, umur, status } = mahasiswa1;
+console.log("nama: " + nama + ", umur: " + umur); // 'nama= budi santoso
+
+console.log(`nama: ${nama}, umur: ${umur}`);
